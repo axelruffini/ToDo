eca628f91e8b8e9fa15be3205ae3ee7c5ddb15d0
axelruffini <axel0064@gmail.com>
2023-06-27 23:48:41 -0300

    Boton de borrar

--- js/main.js
+++ js/main.js
@@ -6,17 +6,28 @@ formulario.addEventListener('submit', (evento) => { //Se carga la tarea
   evento.preventDefault();
   const nombreTareaTexto = nombreTareaInput.value.trim();
 
-  if (nombreTareaTexto) { //Comprueba si se escribio algo
+  if (nombreTareaTexto) { //Comprueba si se escribió algo
     const tareaDiv = document.createElement('div');
     const nombreTareaNode = document.createElement('h3');
     nombreTareaNode.textContent = nombreTareaTexto;
     tareaDiv.appendChild(nombreTareaNode);
-    tareaDiv.classList.add('contenedor-tarjeta');
+    tareaDiv.classList.add('contenedor-tarjeta'); //se agrega la clase contenedora para agregar diseño en cada una de las tareas (se la pone en esta linea asi se agrega al instante de cargarla)
+    
+    // Agregar botón de eliminar con diseño personalizado
+    const botonEliminar = document.createElement('button');
+    botonEliminar.innerHTML = '<img src="./assets/img/delete_FILL0_wght400_GRAD0_opsz48.svg" alt="Eliminar" class="btn-eliminar">'; //Imagen svg del boton borrar
+    botonEliminar.addEventListener('click', () => {
+      eliminarTarea({ nombre: nombreTareaTexto });
+    });
+    tareaDiv.appendChild(botonEliminar);
+    
     contenedorTareas.appendChild(tareaDiv);
     guardarTareaEnStorage(nombreTareaTexto);
     nombreTareaInput.value = '';
   }
 });
+
+
 
 function guardarTareaEnStorage(nombreTarea) { //se la guada en el storage
   const tareas = obtenerTareasDelStorage();
@@ -42,9 +53,37 @@ function mostrarTareas(tareas) { //Se enfoca en la creación de elementos HTML y
     nombreTareaNode.textContent = tarea.nombre;
     tareaDiv.appendChild(nombreTareaNode);
     tareaDiv.classList.add('contenedor-tarjeta'); // Agrega la clase "contenedor-tarjeta" al div de la tarea
+
+        // agrego el botón de eliminación
+        const botonEliminar = document.createElement('button');
+        botonEliminar.textContent = 'Eliminar';
+        botonEliminar.innerHTML = '<img src="./assets/img/delete_FILL0_wght400_GRAD0_opsz48.svg" alt="Eliminar" class="btn-eliminar">'; //Imagen svg del boton borrar
+        botonEliminar.addEventListener('click', () => {
+          eliminarTarea(tarea);
+        });
+        tareaDiv.appendChild(botonEliminar);
+
     contenedorTareas.appendChild(tareaDiv);
   }
 }
+
+function eliminarTarea(tarea) {
+  const tareas = obtenerTareasDelStorage();
+  const indice = tareas.findIndex((t) => t.nombre === tarea.nombre);
+  if (indice !== -1) {
+    tareas.splice(indice, 1);
+    guardarTareasEnStorage(tareas);
+    limpiarContenedorTareas();
+    mostrarTareas(tareas);
+  }
+}
+
+function limpiarContenedorTareas() {
+  while (contenedorTareas.firstChild) {
+    contenedorTareas.removeChild(contenedorTareas.firstChild);
+  }
+}
+
 
 function cargarTareas() { //Coordina la obtención y visualización de las tareas
   const tareasLocalStorage = obtenerTareasDelStorage();
@@ -52,3 +91,4 @@ function cargarTareas() { //Coordina la obtención y visualización de las tarea
 }
 
 window.addEventListener('load', cargarTareas); //Ejecuta la funcion cargarTareas()
+
