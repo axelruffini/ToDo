0b0c1fbe69ba97997c4e7cd90f2146fc370656b8
axelruffini <axel0064@gmail.com>
2023-07-26 16:22:48 -0300

    JS modificado para mejor rendimiento

--- js/main.js
+++ js/main.js
@@ -2,62 +2,50 @@
 function addTask() {
   const taskInput = document.getElementById('taskInput');
   const taskText = taskInput.value.trim();
-
-  if (taskText !== '') {
-    // Obtener tareas existentes de LocalStorage o inicializar un array vacío si es la primera vez
-    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-    // Agregar la nueva tarea al array
+  if (taskText !== '') { 
+    let tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
     tasks.push(taskText);
-
-    // Guardar el array actualizado en LocalStorage
     localStorage.setItem('tasks', JSON.stringify(tasks));
-
-    // Limpiar el input y actualizar la lista de tareas mostradas
     taskInput.value = '';
-    displayTasks();
+    displayTasks(tasks);
   }
 }
 
-// Función para mostrar las tareas en el main
-function displayTasks() {
+ // Función para mostrar las tareas en el main
+function displayTasks(tasks) {
   const taskList = document.getElementById('taskList');
-  taskList.innerHTML = '';
-
-  // Obtener las tareas de LocalStorage
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-  // Mostrar cada tarea en el main
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
   tasks.forEach((task, index) => {
     const taskElement = document.createElement('div');
-    taskElement.innerHTML = `
-    <div class="nueva-tarjeta">
-      <span>${task}</span>
-        <button onclick="deleteTask(${index})" class="btn">
-          <img src="./assets/img/Eliminar.svg" alt="">
-        </button>
-    </div>
-    `;
+    taskElement.classList.add('nueva-tarjeta');
+    const taskText = document.createElement('span');
+    taskText.textContent = task;
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('btn');
+    deleteButton.addEventListener('click', () => deleteTask(index));
+    const deleteImage = document.createElement('img');
+    deleteImage.src = './assets/img/Eliminar.svg';
+    deleteImage.alt = '';
+    deleteButton.appendChild(deleteImage);
+    taskElement.appendChild(taskText);
+    taskElement.appendChild(deleteButton);
     taskList.appendChild(taskElement);
   });
 }
 
-// Función para eliminar una tarea
+ // Función para eliminar una tarea
 function deleteTask(index) {
-  // Obtener las tareas de LocalStorage
-  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-  // Eliminar la tarea del array según el índice proporcionado
+  let tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
   if (index >= 0 && index < tasks.length) {
     tasks.splice(index, 1);
   }
-
-  // Guardar el array actualizado en LocalStorage
   localStorage.setItem('tasks', JSON.stringify(tasks));
-
-  // Actualizar la lista de tareas mostradas
-  displayTasks();
+  displayTasks(tasks);
 }
 
-// Mostrar las tareas al cargar la página
-displayTasks();
+
+ // Mostrar las tareas al cargar la página
+let tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+displayTasks(tasks);
